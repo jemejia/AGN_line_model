751b16e00c6c869ea65582bcf6fb67ed5f5a773c
jemejia <julianmejia@gmail.com>
2014-02-19 12:51:41 -0300

    balmer continuum properly computed

--- fitcode/line.py
+++ fitcode/line.py
@@ -24,9 +24,11 @@ def continuous_substraction( spec_number, sp, magorder,cont_limits):
     cont=exclude_cont[spec_number,:][1:]
     print "cont=  ", cont
     arg_min=np.argmin( np.abs( cont - cont_limits[0] ) )
-    if cont[arg_min] - cont_limits[0] > 0 : arg_min= arg_min + 1
+    if cont[arg_min] - cont_limits[0] < 0 : arg_min= arg_min + 1
     arg_max=np.argmin( np.abs( cont - cont_limits[1] ) )
-    if cont[arg_max] - cont_limits[0] < 0 : arg_max= arg_max - 1
+    if cont[arg_max] - cont_limits[0] > 0 : arg_max= arg_max - 1
+    if len(cont[arg_min:arg_max+1]) % 2 == 1: arg_max=arg_max + 1
+    print len(cont[arg_min:arg_max+1]), len(cont[arg_min:arg_max+1]) % 2
     cont=cont[arg_min:arg_max+1]
     
     print "cont=  ", cont
@@ -51,16 +53,133 @@ def continuous_substraction( spec_number, sp, magorder,cont_limits):
     
     
     Fmodel*=mean_ratio
+            
     
+    return Fmodel, cont[0]
+
+
+#sp (the spectrum) must be given after global continuous has been substracted
+def balmer_normalization(sp):
+    
+    template_file="./templates/balmer_template4.txt"
+    balmer_lines_file="./templates/balmer_lines4.txt"
+    balmer_template = np.loadtxt(template_file)
+    balmer_lines=np.loadtxt(balmer_lines_file)
+    balmer_data=np.interp(sp.xarr,balmer_template[:,0], balmer_template[:,1])
+    balmer_lines_data=np.interp(sp.xarr,balmer_lines[:,0], balmer_lines[:,1])
+    balmer_template=pyspeckit.Spectrum(data=balmer_data,xarr=sp.xarr)
+    balmer_template.xarr.units='angstroms'
+    balmer_template.xarr.xtype='wavelength'
+    balmer_lines=pyspeckit.Spectrum(data=balmer_lines_data,xarr=sp.xarr)
+    balmer_lines.xarr.units='angstroms'
+    balmer_lines.xarr.xtype='wavelength'
+    
+    lambda_balmer=3646
+    index=np.argmin( np.abs( lambda_balmer - sp.xarr) )
+    index_hi=np.argmin( np.abs( 4000.0 - sp.xarr) )
+    flux=np.mean(sp.data[ index - 30 : index + 30 ])
+    balmer_lines.data*=flux/balmer_template.data[-1]
+    balmer_template.data*=flux/balmer_template.data[-1]
+            
+    balmer_tot=np.zeros(len(balmer_template.data))
+    balmer_tot[:index]=balmer_template.data[:index]
+    balmer_tot[index:index_hi]=balmer_lines.data[index:index_hi]
+    
+    
+    return balmer_template, balmer_tot, index
+    
+def total_continuous_fit(sp,continuous,balmer,magorder):
+    
+
+    
+    #-----------------------------defining balmer template fitter ------------------------------------#
+    
+
+        
+    def total_cont(xarr, scale_cont,scale_balmer,cont=continuous.copy(), balm=balmer.copy()):
+            
+            
+        total_continuous =pyspeckit.interpolation._interp(xarr.as_unit('angstroms'), 
+                                                          cont.xarr.as_unit('angstroms'),
+                                                          scale_cont*cont.data + scale_balmer*balm.data)
+        
+                    
+        return total_continuous
+    
+        
+    modelclass = pyspeckit.spectrum.models.model.SpectralModel(total_cont,2, 
+                                                          parnames=['scale_cont','scale_balmer'],
+                                                          parlimited=[(True,True),(True,True)],
+                                                          parlimits=[(0.8,1.2),(0.8,1.2)],
+                                                          shortvarnames=('Ac','Ab') 
+                                                          )
+    modelclass.__name__ ="contmodel"
+        
+    xmin=2150
+    xmax=3670
+    #-----------restarting plotter------------#
+    sp.plotter.refresh()    
+    #sp.plotter.reset_limits()
+    sp.plotter(xmin=xmin,xmax=xmax)
+    #-----------restarting plotter------------#
+        
+    exclude=[1000,2190,2200,3641,3651,10000]
+    
+    
+    
+        
+    #sp.Registry.add_fitter('total',total,2,multisingle='milti')
+    #sp.specfit(fittype='al',exclude=exclude,guesses=[0.99,0.99],xmin=xmin,xmax=xmax)
+    
+
+        
+    sp.Registry.add_fitter('contmodel',modelclass,2,multisingle='multi')
+    sp.specfit(fittype='contmodel',exclude=exclude,guesses=[1.0,1.0],xmin=xmin,xmax=xmax)
+    
+
+    sp.specfit.plot_components(add_baseline=False)
+    sp.plotter.refresh()
+    sp.specfit.plot_fit(annotate=False)
+    
+    # -----------------  fitting ---------------------
+        
+    # -----------------  ploting  fit---------------------
+
+    
+    fitmin=sp.specfit.xmin
+    fitmax=sp.specfit.xmax
+    sp.specfit.fullsizemodel() 
+    print sp.specfit.parinfo.values[1], sp.specfit.parinfo.values[0], sp.specfit.xmin, sp.specfit.xmax
+    
+    #--------------rescaling balmer and continuous----------
+    continuous.data*=sp.specfit.parinfo.values[0]
+    balmer.data*=sp.specfit.parinfo.values[1]
+    
+
+
+    #plot_file="./plots/new_cont" + line_name + "_" + str(spec_number) + ".png"
+    plot_file="./plots/new_cont.png"
     pylab.figure()
-    pylab.ylabel(r'$10^{'+str(magorder)+'}$ erg s$^{-1}$ $cm^{-1}$')
+    #pylab.ylim(ymin=-1.2*np.abs(sp.data[fitmin:fitmax].min()),ymax=2*sp.specfit.residuals[fitmin:fitmax].max())
+    #pylab.xlim(xmin=xmin-300,xmax=xmax)
+    pylab.ylabel(r'$10^{'+str(magorder)+'}$ erg s$^{-1}$  $cm^{-1}$')
     pylab.xlabel(r'$\AA$')
     pylab.plot(sp.xarr,sp.data,'k')
-    pylab.plot(sp.xarr,Fmodel,'r')
-    pylab.show()
+    pylab.plot(sp.xarr,sp.specfit.fullmodel,'r')
+    pylab.savefig(plot_file)
+    #pylab.close()
+    # -----------------  ploting fit---------------------
 
-    sp.data=sp.data - Fmodel 
-    return Fmodel, cont[arg_min]
+
+    
+    model=sp.specfit.fullmodel
+
+    return continuous, balmer
+
+
+
+
+
 
 def smooth(data,smooth,smoothtype='gaussian',downsample=False,downsample_factor=None,
         convmode='same'):
@@ -81,6 +200,7 @@ def smooth(data,smooth,smoothtype='gaussian',downsample=False,downsample_factor=
         see :mod:`numpy.convolve`.  'same' returns an array of the same length as
         'data' (assuming data is larger than the kernel)
     """
+
     
     roundsmooth = smooth # can only downsample by integers
 
@@ -147,10 +267,8 @@ def wl_to_kms(wl,line_center):
 def blue_bump_fitter(sp,line_name,spec_number,xmin,xmax,fe_template, magorder):
     
     #-----------------------------defining fe template fitter ------------------------------------#
-    #template_file="./templates/fe2_UV.dat"
-    #template = pyspeckit.Spectrum(template_file)
-
-    for i in range(5):
+    
+    for i in range(3):
         
         def model(xarr, scale_fe,shift_fe, smooth_factor_fe):#,scale_balmer):,fe_template=fe_template.copy(), balmer_template=balmer_template.copy()):
         
@@ -158,14 +276,7 @@ def blue_bump_fitter(sp,line_name,spec_number,xmin,xmax,fe_template, magorder):
             new_fe_template = scale_fe*pyspeckit.interpolation._interp(xarr.as_unit('angstroms'), 
                                                                        fe_template.xarr.as_unit('angstroms')+shift_fe, 
                                                                        fe_template.data )
-        
-            #data=smooth(new_fe_template.data,smooth_factor_fe , downsample=False    )
-            #new_fe_template.data=data
-            #new_balmer_template = scale_balmer*pyspeckit.interpolation._interp(xarr.as_unit('angstroms'), 
-            #                                         balmer_template.xarr.as_unit('angstroms'), 
-            #                                         balmer_template.data)
-            
-
+           
             new_template=  new_fe_template #+ new_balmer_template 
             return new_fe_template
 
@@ -196,6 +307,7 @@ def blue_bump_fitter(sp,line_name,spec_number,xmin,xmax,fe_template, magorder):
         
         
         # -----------------  fitting ---------------------
+        
         exclude_file = "./excludes/exclude_" + line_name + ".txt"
         if os.path.exists(exclude_file) and os.path.isfile(exclude_file):
             exclude_data=np.loadtxt(exclude_file,skiprows=2)
@@ -223,10 +335,7 @@ def blue_bump_fitter(sp,line_name,spec_number,xmin,xmax,fe_template, magorder):
                                                         templat.xarr.as_unit('angstroms'), 
                                                         templat.data)
             fe_template.data=data
-            
-            #new_template=  fe_template #+ new_balmer_template 
-            
-            
+                    
             return n_template
     
         
@@ -249,6 +358,7 @@ def blue_bump_fitter(sp,line_name,spec_number,xmin,xmax,fe_template, magorder):
     fitmin=sp.specfit.xmin
     fitmax=sp.specfit.xmax
     sp.specfit.fullsizemodel() 
+    
     plot_file="./plots/fe_fit_" + line_name + "_" + str(spec_number) + ".png"
     pylab.figure()
     pylab.ylim(ymin=-1.2*np.abs(sp.data[fitmin:fitmax].min()),ymax=2*sp.specfit.residuals[fitmin:fitmax].max())
@@ -258,6 +368,7 @@ def blue_bump_fitter(sp,line_name,spec_number,xmin,xmax,fe_template, magorder):
     pylab.plot(sp.xarr,sp.data,'k')
     pylab.plot(sp.xarr,sp.specfit.fullmodel,'r')
     pylab.savefig(plot_file)
+    #pylab.close()
     # -----------------  ploting fit---------------------
     
     
@@ -271,15 +382,15 @@ def blue_bump_fitter(sp,line_name,spec_number,xmin,xmax,fe_template, magorder):
     
     pylab.plot(sp.xarr[fitmin:fitmax],sp.specfit.residuals[fitmin:fitmax],'k')
     plot_file="./plots/" + line_name +"_res_" + str(spec_number) + ".png"
-    
     pylab.savefig(plot_file)
+    #pylab.close()
     # ----------plotting residuals-------------#
     
     #----------redifining spectrum ------------------#
     sp.data=sp.data - sp.specfit.fullmodel
     #----------redifining spectrum ------------------#
     return sp.specfit.fullmodel
-        
+
 def line_fitter(sp,line_name, spec_number,guesses, limits, limited, tied, xmin, xmax, offset=-0.4, magorder=-16):
     #-----------restarting plotter------------#
     sp.plotter.refresh()    
@@ -300,7 +411,7 @@ def line_fitter(sp,line_name, spec_number,guesses, limits, limited, tied, xmin, 
    
     # ----------------- fit plotting  ---------------------
     sp.specfit.plot_components(add_baseline=False)
-    
+    pylab.close()
     sp.plotter.refresh()
     sp.specfit.plot_fit(annotate=False)
     # -----------------  fit plotting  ---------------------
@@ -313,7 +424,7 @@ def line_fitter(sp,line_name, spec_number,guesses, limits, limited, tied, xmin, 
     # ----------plotting residuals-------------
     plot_file="./plots/" + line_name + "_" + str(spec_number) + ".png"
     sp.plotter.figure.savefig(plot_file)
-    
+    pylab.close()
     fitmin=sp.specfit.xmin
     fitmax=sp.specfit.xmax
     pylab.figure()
@@ -325,11 +436,12 @@ def line_fitter(sp,line_name, spec_number,guesses, limits, limited, tied, xmin, 
     plot_file="./plots/" + line_name +"_res_" + str(spec_number) + ".png"
     
     pylab.savefig(plot_file)
+    pylab.close()
     # ----------plotting residuals-------------#
     
     
     #---------------measuring----------------------#
-    sp.measure(z = 1.51831, fluxnorm = 10**magorder)
+    sp.measure(z = 0.0, fluxnorm = 10**magorder)
 
     for i, line in enumerate(sp.measurements.lines.keys()):
 
@@ -350,29 +462,24 @@ def line_fitter(sp,line_name, spec_number,guesses, limits, limited, tied, xmin, 
         print_line = '{0}  {1}  {2}  {3} {4} {5}\n'.format(line, sp.measurements.lines[line]['flux'],sp.measurements.lines[line]['amp'],sp.measurements.lines[line]['fwhm'],sp.measurements.lines[line]['lum'], sp.measurements.lines[line]['modelpars'][1] )
         f.write(print_line)
     f.close()
-    return sp.specfit.fullmodel
     #---------------measuring----------------------#
+    return sp.specfit.fullmodel
+    
 
 
 def fe_fitter(sp,line_name,spec_number,xmin,xmax,template,magorder):
     
     #-----------------------------defining fe template fitter ------------------------------------#
-    #template_file="./templates/fe2_UV.dat"
-    #template = pyspeckit.Spectrum(template_file)
-
+    
     for i in range(5):
         
         def fe_model(xarr, scale_fe,shift_fe, smooth_factor_fe):
-
-            
 
             new_fe_template = scale_fe*pyspeckit.interpolation._interp(xarr.as_unit('angstroms'), 
                                                                        template.xarr.as_unit('angstroms')+shift_fe, 
                                                                        template.data )
         
-            
-            
-
+    
             new_template=  new_fe_template 
             return new_fe_template
 
@@ -458,11 +565,12 @@ def fe_fitter(sp,line_name,spec_number,xmin,xmax,template,magorder):
     pylab.figure()
     pylab.ylim(ymin=-1.2*np.abs(sp.data[fitmin:fitmax].min()),ymax=2*sp.specfit.residuals[fitmin:fitmax].max())
     pylab.xlim(xmin=xmin-300,xmax=xmax)
-    pylab.ylabel(r'$10^{'+str(mag_order)+'}$ erg s$^{-1}$  $cm^{-1}$')
+    pylab.ylabel(r'$10^{'+str(magorder)+'}$ erg s$^{-1}$  $cm^{-1}$')
     pylab.xlabel(r'$\AA$')
     pylab.plot(sp.xarr,sp.data,'k')
     pylab.plot(sp.xarr,sp.specfit.fullmodel,'r')
     pylab.savefig(plot_file)
+    #pylab.close()
     # -----------------  ploting fit---------------------
     
     
@@ -478,6 +586,7 @@ def fe_fitter(sp,line_name,spec_number,xmin,xmax,template,magorder):
     plot_file="./plots/" + line_name +"_res_" + str(spec_number) + ".png"
     
     pylab.savefig(plot_file)
+    #pylab.close()
     # ----------plotting residuals-------------#
 
     wlmin=np.amin(np.abs(sp.xarr-xmin))
